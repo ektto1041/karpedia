7f1b432cc374fb99758bbc24db8ea328b4af7245
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-02-18 21:59:06 +0900

    feat: title, topic input & 작성 버튼 추가

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import styles from '../../styles/NewPost.module.css';
@@ -17,18 +17,52 @@ const MDViewer = dynamic(
 );
 
 export default function NewPost() {
+  const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
+  const [topic, setTopic] = useState("");
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
 
   const handleChangeContent = (value: Content) => {
     setValue(value!);
-  }
+  };
+
+  const handleChangeTopic = (e: ChangeEvent<HTMLInputElement>) => {
+    setTopic(e.target.value);
+  };
 
   return (
     <div className={styles.container}>
+      <input
+        className={styles.title}
+        type='text'
+        value={title}
+        onChange={handleChangeTitle}
+        placeholder="제목을 입력해주세요."
+      />
       <MDEditor
         value={value}
         onChange={handleChangeContent}
+        visiableDragbar={false}
+        height={500}
+        style={{ marginBottom: '10px' }}
       />
+      <div className={styles['button-box']}>
+        <div className={styles.topic}>
+          <div className={styles.label}>
+            토픽:
+          </div>
+          <input
+            type='text'
+            placeholder="토픽을 입력해주세요."
+            onChange={handleChangeTopic}
+          />
+        </div>
+        <button className={styles['submit-button']}>작성</button>
+      </div>
+      <div style={{ marginTop: '50px' }}>{'<< 미리보기 >>'}</div>
       <div className={styles.viewer} >
         <MDViewer source={value}></MDViewer>
       </div>
