c15a5b4d28b157941e31a18c6901e945e5d6d0d2
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-02-22 14:57:54 +0900

    feat: posts 페이지 추가 / topic-box 추가

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,27 @@
+import Topic from '@/components/posts/topic';
+import styles from '@/styles/Posts.module.css';
+
+export default function Posts() {
+  return (
+    <div className={styles.container}>
+      <div className={styles['topic-box']}>
+        <div className={styles.title}>
+          Topics
+          <button>모두 취소</button>
+        </div>
+        <div className={styles.content}>
+          <Topic>Javascript & React</Topic>
+          <Topic>React</Topic>
+          <Topic>C</Topic>
+          <Topic>Javascript</Topic>
+          <Topic>Next.js</Topic>
+          <Topic>Firestore</Topic>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// export async function getStaticProps() {
+
+// };
