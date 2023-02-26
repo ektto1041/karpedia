73d20809f8fb61893af78a475f0dc3313040e231
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-02-26 15:22:51 +0900

    feat: PostDoc, TopicDoc 등 타입 추가

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,3 +1,27 @@
+import { Timestamp } from "firebase/firestore";
+
 export type PostsProps = {
   topics: string[],
+  postItems: PostItemType[],
 };
+
+export type PostDoc = {
+  emoji: string,
+  title: string,
+  content: string,
+  topics: string[],
+  viewCount: number,
+  comments: any[],  // TODO: 댓글 collection 계획 전까지는 any[]
+  createdAt: Timestamp,
+  modifiedAt: Timestamp,
+}
+
+export type TopicDoc = {
+  name: string,
+}
+
+export type PostItemType = {
+  emoji: string,
+  title: string,
+  modifiedAt: string,
+}
