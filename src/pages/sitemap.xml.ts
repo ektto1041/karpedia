a65ef89eb276e523cf77e10147ef27dd6333ba17
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-05-23 16:19:14 +0900

    feat: seo

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,34 @@
+import { apis } from "@/utils/api";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+
+export default function SiteMap() {}
+
+export async function getServerSideProps({ res }: GetServerSidePropsContext) {
+  const result = await apis.getAllPost();
+  const allPostId = result.data.map(post => post.id);
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(generateSiteMap(allPostId));
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+function generateSiteMap(allPostId: number[]): string {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.karpedia.site</loc>
+    </url>
+    ${allPostId.map((postId) => {
+        return `
+    <url>
+        <loc>${`${process.env.NEXT_PUBLIC_CLIENT_BASE_URL}posts/${postId}`}</loc>
+    </url>`;
+      })
+      .join('')}
+  </urlset>
+  `;
+}
