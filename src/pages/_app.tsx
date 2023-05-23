b430ce02e32f6a10f442c6f9f6cad4369a1e4715
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-05-23 17:25:34 +0900

    fix: add naver verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Karpedia</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="naver-site-verification" content="583881dd6e1c6455ab1e938bd4797cc041751d5a" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavigationBar />
