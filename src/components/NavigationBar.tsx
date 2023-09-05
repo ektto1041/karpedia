f8b1a870124242a2d8c3c4a430d5939e3aed2b80
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-09-05 14:48:28 +0900

    feat: add navBar
    검색창은 나중에 구현

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,15 +4,34 @@ import { mdiAccountBox } from '@mdi/js';
 import { mdiListBox } from '@mdi/js';
 import Link from 'next/link';
 
+type MenuItem = {
+  name: string;
+  href: string;
+}
+
+const menuItems: MenuItem[] = [
+  {name: '포트폴리오', href: '/'},
+  {name: '토픽', href: '/topics'},
+]
+
 export default function NavigationBar() {
   return (
     <div className={styles.container}>
-      <Link href='/'>
-        <Icon path={mdiAccountBox} size='100%' />
+      <Link className={styles.logo} href='/'>
+        Karpedia
       </Link>
-      <Link href='/posts'>
-        <Icon path={mdiListBox} size='100%' />
-      </Link>
+      <div className={styles['nav-menu-list']}>
+        {menuItems.map(item => (
+          <Link key={item.name} className={styles['menu-item']} href={item.href}>
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      <div className={styles['nav-button-list']}>
+        <div className={styles['login-button']}>
+          로그인
+        </div>
+      </div>
     </div>
   );
 };
