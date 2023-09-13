7455108053b0a93a094c3b7045f3cc243fb44898
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-09-13 21:35:19 +0900

    fix: 챕터 없을 때 포스트 작성 못하게 수정

--- src/screens/post/new/TypeSlide.tsx
+++ src/screens/post/new/TypeSlide.tsx
@@ -1,13 +1,24 @@
+import { useCallback } from 'react';
 import { PostType } from '.';
 import styles from './TypeSlide.module.css';
 
 type TypeSlideProps = {
   onClickType: (type: PostType) => void;
+  hasChapter: boolean;
 };
 
 export default function TypeSlide({
   onClickType,
+  hasChapter,
 }: TypeSlideProps) {
+  const onClickPost = useCallback(() => {
+    if(hasChapter) {
+      onClickType('post');
+    } else {
+      alert('챕터가 없습니다. 챕터를 먼저 작성해주세요.');
+    }
+  }, [hasChapter]);
+
   return (
     <div className={styles.container}>
       <h1>어떤 포스트를 작성하시나요?</h1>
@@ -15,7 +26,7 @@ export default function TypeSlide({
         <div className={styles['type-item']} onClick={() => onClickType('category')} >
           카테고리
         </div>
-        <div className={styles['type-item']} onClick={() => onClickType('post')} >
+        <div className={styles['type-item']} onClick={onClickPost} >
           포스트
         </div>
       </div>
