01e28f1e5dcea958594f283da7ed912056703cd5
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-09-15 01:53:38 +0900

    feat: 포트폴리오 페이지 수정

--- src/screens/portfolio/ProjectItem.tsx
+++ src/screens/portfolio/ProjectItem.tsx
@@ -9,6 +9,7 @@ export type Project = {
   date: string,
   description: JSX.Element,
   icons: string[],
+  histories: string[],
 };
 
 type ProjectItemProps = {
@@ -18,7 +19,7 @@ type ProjectItemProps = {
 export default function ProjectItem({
   project,
 }: ProjectItemProps) {
-  const {bgImg, href, title, date, icons, description} = project;
+  const {bgImg, href, title, date, icons, description, histories} = project;
 
   return (
     <div className={styles.container}>
@@ -26,20 +27,30 @@ export default function ProjectItem({
         {/* <Image src={bgImg} alt='project-img' width={700} height={400}/> */}
         <Image src={bgImg} alt='project-img' fill/>
       </div>
-      <div className={styles.description}>
+      <div className={styles.info}>
         <div className={styles.title} >
           {title}
-        </div>
-        <div className={styles.date} >
-          {date}
+          <div className={styles.date} >
+            {date}
+          </div>
         </div>
         <div className={styles['icon-row']} >
           {icons?.map(icon => (
             <TechItem key={icon} src={`${icon}`} />
           ))}
         </div>
-        <div className={styles.desc} >
+        <div className={styles.description} >
           {description}
+        </div>
+        <div className={styles.histories} >
+          진행내용:
+          <ul>
+            {histories?.map(history => (
+              <li key={history} className={styles.history}>
+                {history}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
