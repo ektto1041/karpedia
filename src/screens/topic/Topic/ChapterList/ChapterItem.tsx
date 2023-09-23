df1bdc892f6b80e58a568a52aa502dd2a7be8c07
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-09-23 14:22:32 +0900

    feat: 보고 있는 포스트or챕터 강조

--- src/screens/topic/Topic/ChapterList/ChapterItem.tsx
+++ src/screens/topic/Topic/ChapterList/ChapterItem.tsx
@@ -1,26 +1,35 @@
 import { ChaptersWithPostsDto } from '@/types/chapter';
 import styles from './ChapterItem.module.css';
 import PostList from './PostList';
+import css from '@/utils/css';
+import { useMemo } from 'react';
 
 type ChapterItemProps = {
   chapter: ChaptersWithPostsDto;
+  pathChapterId: number;
+  pathPostId: number;
   onClickChapter: (chapterId: number) => void;
   onClickPost: (postId: number) => void;
 };
 
 export default function ChapterItem({
   chapter,
+  pathChapterId,
+  pathPostId,
   onClickChapter,
   onClickPost,
 }: ChapterItemProps) {
   const {id, title, content, postsList} = chapter;
+  const isCurrent = useMemo(() => {
+    return pathPostId === -1 && (id === pathChapterId);
+  }, [id, pathPostId, pathChapterId]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.title} onClick={() => {onClickChapter(id)}}>
+      <div className={css(styles.title, isCurrent ? styles.current : '')} onClick={() => {onClickChapter(id)}}>
         {title}
       </div>
-      <PostList postList={postsList} onClickPost={onClickPost} />
+      <PostList postList={postsList} pathPostId={pathPostId} onClickPost={onClickPost} />
     </div>
   );
 };
--- src/screens/topic/Topic/ChapterList/PostItem.tsx
+++ src/screens/topic/Topic/ChapterList/PostItem.tsx
@@ -1,19 +1,22 @@
 import { PostsDto } from '@/types/post';
 import styles from './PostItem.module.css';
+import css from '@/utils/css';
 
 type PostItemProps = {
   post: PostsDto;
+  pathPostId: number;
   onClickPost: (postId: number) => void;
 };
 
 export default function PostItem({
   post,
+  pathPostId,
   onClickPost,
 }: PostItemProps) {
   const {id, title, content, status, viewCount, createdAt, modifiedAt,} = post;
   
   return (
-    <div className={styles.container} onClick={() => onClickPost(id)}>
+    <div className={css(styles.container, id === pathPostId ? styles.current : '')} onClick={() => onClickPost(id)}>
       {title}
     </div>
   );
