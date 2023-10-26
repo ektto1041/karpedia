c6121f9243c002bc167d6ae2752f2cda2a5f027e
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-10-26 13:28:09 +0900

    feat: 포트폴리오 페이지 meta tag 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import PortfolioScreen from '@/screens/portfolio/Portfolio'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Karpedia</title>
+        <meta name='description' content='웹 개발 블로그 Karpedia 입니다.' />
+      </Head>
       <PortfolioScreen />
     </>
   )
