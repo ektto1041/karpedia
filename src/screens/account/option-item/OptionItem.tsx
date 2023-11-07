477f06a3fdcfc4f54a0b1ee045ad72ab872cb98d
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2023-11-07 23:49:53 +0900

    feat: 프로필 이미지 업로드 추가

--- src/screens/account/option-item/OptionItem.tsx
+++ src/screens/account/option-item/OptionItem.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import styles from './OptionItem.module.css';
 
 type OptionItemButton = {
   label: string;
+  disabled: boolean;
   onClick: () => void;
 };
 
 export type OptionItemProps = {
   name: string;
-  description: string;
+  description: string[];
   children: JSX.Element;
   buttons: OptionItemButton[];
 }
@@ -22,14 +24,20 @@ export default function OptionItem({
     <section className={styles.container}>
       <div className={styles.main}>
         <h1>{name}</h1>
-        <h2>{description}</h2>
+        <h2>
+        {description.map((line, idx) => (
+          <React.Fragment key={idx}>
+            {line}<br />
+          </React.Fragment>
+        ))}
+        </h2>
         <div className={styles.content}>
           {children}
         </div>
       </div>
       <div className={styles['button-box']}>
         {buttons.map(button => (
-          <button key={button.label} onClick={button.onClick}>{button.label}</button>
+          <button key={button.label} onClick={button.onClick} disabled={button.disabled}>{button.label}</button>
         ))}
       </div>
     </section>
--- src/screens/account/profile-tab/ProfileTab.tsx
+++ src/screens/account/profile-tab/ProfileTab.tsx
@@ -1,18 +1,96 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import OptionItem from '../option-item/OptionItem';
 import styles from './ProfileTab.module.css';
+import Image from 'next/image';
+import useAppSelector from '@/hooks/useAppSelector';
+import { selectSelfUser } from '@/redux/slices/AuthSlice';
+import { apis } from '@/utils/api';
 
 export default function ProfileTab() {
-  const [newUsername, setNewUsername] = useState('박상연'); 
+  const selfUser = useAppSelector(selectSelfUser);
+  
+  const [oldProfileImage, setOldProfileImage] = useState<string>();
+  const [newProfileImage, setNewProfileImage] = useState<string>();
+  const isTabReady = Boolean(newProfileImage);
+
+  useEffect(() => {
+    if(selfUser) {
+      setOldProfileImage(selfUser.profileImage);
+      setNewProfileImage(selfUser.profileImage);
+    } else {
+      setOldProfileImage(undefined);
+      setNewProfileImage(undefined);
+    }
+  }, [selfUser]);
+
+  const onClickSetImage = useCallback(() => {
+    const imageInput = document.createElement('input');
+    imageInput.type = 'file';
+    imageInput.setAttribute('style', 'display: none;');
+    imageInput.accept = 'image/*';
+    imageInput.addEventListener('change', async (e) => {
+      const target = e.target as HTMLInputElement;
+      const file = target.files ? target.files[0] : null;
+
+      if(file) {
+        const MAX_SIZE = 1000000;
+        if(file.size <= MAX_SIZE) {
+          const formData = new FormData();
+          formData.append('file', file);
+
+          const response = await apis.uploadImageToS3(formData);
+          if(response.status < 300) {
+            setNewProfileImage(response.data);
+
+          } else if(response.status === 500) {
+            alert('서버 오류로 인해 이미지 업로드에 실패했습니다.');
+          }
+        } else {
+          alert('이미지 파일의 용량은 1MB 를 초과할 수 없습니다.');
+        }
+      } else {
+        alert('이미지 파일을 선택하지 않았습니다.');
+      }
+
+      document.body.removeChild(imageInput);
+    });
+    imageInput.addEventListener('cancel', () => {
+      alert('이미지 파일을 선택하지 않았습니다.');
+
+      document.body.removeChild(imageInput);
+    })
+
+    document.body.appendChild(imageInput);
+    imageInput.click();
+  }, []);
+
+  const onClickSaveProfileImage = useCallback(() => {
+
+  }, []);
 
   return (
     <div className={styles.container} >
-      <OptionItem name='프로필 이미지 수정' description='사용자들에게 보여질 프로필 이미지를 수정합니다.' buttons={[{ label: '저장', onClick: () => {} }]}>
-        <></>
-      </OptionItem>
-      <OptionItem name='프로필 정보 수정' description='닉네임을 수정합니다.' buttons={[{ label: '저장', onClick: () => {} }]}>
-        <input type='text' placeholder={newUsername} />
-      </OptionItem>
+      {isTabReady ? (
+        <>
+          <OptionItem
+            name='프로필 이미지 수정'
+            description={['사용자들에게 보여질 프로필 이미지를 수정합니다.', '1MB 이하의 이미지 파일만 가능합니다.']}
+            buttons={[{ label: '저장', disabled: oldProfileImage === newProfileImage, onClick: () => {} }]}
+          >
+            <div className={styles['profile-image-box']} onClick={onClickSetImage}>
+              <div className={styles['comment-size-image']}>
+                <Image src={newProfileImage!} alt='댓글 프로필 이미지' fill />
+              </div>
+              <div className={styles['nav-bar-size-image']}>
+                <Image src={newProfileImage!} alt='탐색창 프로필 이미지' fill />
+              </div>
+            </div>
+          </OptionItem>
+          <OptionItem name='프로필 정보 수정' description={['닉네임을 수정합니다.']} buttons={[{ label: '저장', disabled: true, onClick: () => {} }]}>
+            <input type='text' placeholder={''} />
+          </OptionItem>
+        </>
+      ) : (<></>)}
     </div>
   );
 }
